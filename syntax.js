3b5ea50e0e50b5af7ca88a597b1af9b3ba490667
miyeaier <miyesier@gmail.com>
2021-12-02 00:04:24 +0100

    syntax exercise

---
+++ syntax.js
@@ -0,0 +1,17 @@
+//You can drop off the : and the word function for methods when defining them:
+
+//const obj = {
+ // insteadOfThis: function() {
+    // do stuff
+ // },
+
+  //youCanDoThis() {
+    // do stuff
+  //}
+//}
+const obj ={
+addfuc(){
+  console.log (6-4)
+}
+}
+obj.addfuc()//在叫funtion 的时候必须先叫主class 后叫class 下定义的小class 
